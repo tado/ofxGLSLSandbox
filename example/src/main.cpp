58f922291c4650fc3ca8414173f0219dc72b32b3
Atsushi Tadokoro <tadokoro@gmail.com>
2015-11-11 00:14:51 +0900

    update testApp to ofApp

--- example/src/main.cpp
+++ example/src/main.cpp
@@ -1,16 +1,14 @@
 #include "ofMain.h"
-#include "testApp.h"
+#include "ofApp.h"
 #include "ofAppGlutWindow.h"
 
 //========================================================================
 int main( ){
-
-    ofAppGlutWindow window;
-	ofSetupOpenGL(&window, 1024,640, OF_WINDOW);			// <-------- setup the GL context
-
-	// this kicks off the running of my app
-	// can be OF_WINDOW or OF_FULLSCREEN
-	// pass in width and height too:
-	ofRunApp( new testApp());
-
+  ofAppGlutWindow window;
+  ofSetupOpenGL(&window, 1024,640, OF_WINDOW);			// <-------- setup the GL context
+  
+  // this kicks off the running of my app
+  // can be OF_WINDOW or OF_FULLSCREEN
+  // pass in width and height too:
+  ofRunApp( new ofApp());
 }
--- example/src/ofApp.cpp
+++ example/src/ofApp.cpp
@@ -1,7 +1,7 @@
-#include "testApp.h"
+#include "ofApp.h"
 
 //--------------------------------------------------------------
-void testApp::setup(){
+void ofApp::setup(){
     // create new ofxGLSLSandbox instance
     glslSandbox = new ofxGLSLSandbox();
     
@@ -13,63 +13,63 @@ void testApp::setup(){
 }
 
 //--------------------------------------------------------------
-void testApp::update(){
-
+void ofApp::update(){
+    
 }
 
 //--------------------------------------------------------------
-void testApp::draw(){
+void ofApp::draw(){
     // draw shader
     glslSandbox->draw();
-
+    
     ofSetHexColor(0xffffff);
     ofDrawBitmapString("[f] Toggle fullscreen, [o] Open shader file on external editor, [r] Reload shader", 10, 15);
     ofDrawBitmapString(ofToString(ofGetFrameRate(), 4), 10, 30);
 }
 
 //--------------------------------------------------------------
-void testApp::keyPressed(int key){
+void ofApp::keyPressed(int key){
     // set key actions
     glslSandbox->keyPressed(key);
 }
 
 //--------------------------------------------------------------
-void testApp::keyReleased(int key){
-
+void ofApp::keyReleased(int key){
+    
 }
 
 //--------------------------------------------------------------
-void testApp::mouseMoved(int x, int y ){
+void ofApp::mouseMoved(int x, int y ){
     // update mouse position
     glslSandbox->mouseMoved(mouseX, mouseY);
 }
 
 //--------------------------------------------------------------
-void testApp::mouseDragged(int x, int y, int button){
-
+void ofApp::mouseDragged(int x, int y, int button){
+    
 }
 
 //--------------------------------------------------------------
-void testApp::mousePressed(int x, int y, int button){
-
+void ofApp::mousePressed(int x, int y, int button){
+    
 }
 
 //--------------------------------------------------------------
-void testApp::mouseReleased(int x, int y, int button){
-
+void ofApp::mouseReleased(int x, int y, int button){
+    
 }
 
 //--------------------------------------------------------------
-void testApp::windowResized(int w, int h){
-
+void ofApp::windowResized(int w, int h){
+    
 }
 
 //--------------------------------------------------------------
-void testApp::gotMessage(ofMessage msg){
-
+void ofApp::gotMessage(ofMessage msg){
+    
 }
 
 //--------------------------------------------------------------
-void testApp::dragEvent(ofDragInfo dragInfo){ 
-
+void ofApp::dragEvent(ofDragInfo dragInfo){
+    
 }
